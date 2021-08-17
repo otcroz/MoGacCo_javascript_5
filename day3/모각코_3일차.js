9fbf926dab0a1917b1182e1e21e7cc4db8b8c9fe
SOOYEON YOO <dbtndus0527@gmail.com>
2021-08-17 22:50:44 +0900

    Add files via upload

---
+++ day3/모각코_3일차.js
@@ -0,0 +1,12 @@
+function inputText1(){
+    var div = document.getElementsByTagName('div')
+    div[0].innerHTML="모각코에 참여한만큼"
+}
+function inputText2(){
+    var div = document.getElementsByTagName('div')
+    div[1].innerHTML="각오하고 끝까지 열심히 활동할게요!"
+}
+function inputText3(){
+    var div = document.getElementsByTagName('div')
+    div[2].innerHTML="코린이 탈출을 위해 화이팅~~"
+}
